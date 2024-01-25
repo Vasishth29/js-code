d5f76a302bb98f19b6ad28f876209136706327b0
Vasishth29 <157611630+Vasishth29@users.noreply.github.com>
2024-01-25 11:18:40 +0000

    Datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; //tereats all JS code as newer version
+
+//alert(3+3)// we are using nodejs, not browser
+
+// we should also know that our code should be readable and mind in between gaps and writing code on another line
+
+// like the below
+
+console.log(30+20);
+
+console.log("Vasishth"); 
+
+
+let name = "Vasishth"//string data type
+let age = 18 //number data type
+let isLoggedIn = false //boolean data type
+
+//data types is as follows:-
+//number => 2 to power 53
+//bigint - for large numbers
+//string => ""
+//boolean => true/false
+//null => standalone value or a empty value
+//undefined => value not assigned
+
+//object => VVIP in next vedios
+
+console.log(typeof null);// output object
+console.log(typeof undefined);// output undefined
