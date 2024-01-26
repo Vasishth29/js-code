f9533166065ac19ee8d057272fb549f543b688a0
Vasishth29 <157611630+Vasishth29@users.noreply.github.com>
2024-01-26 10:59:40 +0000

    Cnversion of Datatypes

---
+++ 01_Basics/03_conversionsOperation.js
@@ -0,0 +1,25 @@
+let score = undefined
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+//NOTES:-
+//"56"=> 56
+// "56svvvd"=> NaN [not an number]
+// true=> 1; false=> 0 
+// null=> 0
+//undefined=> NaN
+
+let isLoggedIn ="Vasishth"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//NOTES:-
+//1=> TRUE; 0=> false
+//"Vasishth"=> true  
