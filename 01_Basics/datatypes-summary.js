ec8d32fad19e103587b601e1b06317ff3d0e4d8e
Vasishth29 <157611630+Vasishth29@users.noreply.github.com>
2024-01-26 14:54:05 +0000

    Datatypes Summary

---
+++ 01_Basics/datatypes-summary.js
@@ -0,0 +1,31 @@
+//primitive datatypes
+// 7 types : string(""), Number, Boolean, null, undefined, symbol(used to make a value unique), BigInt
+let bigNumber = 6548484135n;
+
+//Reference (Non Primitive)
+//Array, Object, Functions
+
+//arrays:-
+const heroes = ["RadhaGopinath", "RadhaSyamsundar", "RadhaPandharinath"];
+
+//object:-
+let myObj = {
+    name : "Vasishth",
+    age : 18,
+}
+
+//function:-
+const myFunction = function(){
+    console.log("Hare Krishna");
+}
+
+//How to find Datatype:-
+console.log(typeof bigNumber);
+
+//every Reference typeof is "Functions".
+//Function typeof is called Object "Function".
+
+
+//Read and study:-
+//https://262.ecma-international.org/5.1/#sec-11.4.3
+
