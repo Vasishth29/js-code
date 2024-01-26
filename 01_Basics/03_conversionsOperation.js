68386f52906047a2f14a698a9300c6e3aa9802fc
Vasishth29 <157611630+Vasishth29@users.noreply.github.com>
2024-01-26 12:16:59 +0000

    Operations done

--- 01_Basics/03_conversionsOperation.js
+++ 01_Basics/03_conversionsOperation.js
@@ -18,8 +18,49 @@ let valueInNumber = Number(score)
 let isLoggedIn ="Vasishth"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 //NOTES:-
 //1=> TRUE; 0=> false
 //"Vasishth"=> true  
+
+
+//*************************OPERATIONS***********************************//
+
+let value = 3
+let negvalue = -value
+//console.log(negvalue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%3);
+
+let str1 = "HARE KRISHNA"
+let str2 = " KISHAN PRABHUJI"
+
+let str3 = str1 + str2
+
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+console.log(+true);
+console.log(+"");
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter); 
+
+// study and analyize and revise and code this :-
+//https://262.ecma-international.org/#sec-abstract-operations
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+
+
