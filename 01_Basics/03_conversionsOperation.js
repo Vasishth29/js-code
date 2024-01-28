5b4c694d80425268e2b8dbfb421c0cc29499d4fd
Vasishth29 <157611630+Vasishth29@users.noreply.github.com>
2024-01-28 10:02:19 +0000

    Conversions and Operation

--- 01_Basics/03_conversionsOperation.js
+++ 01_Basics/03_conversionsOperation.js
@@ -50,7 +50,7 @@ let str3 = str1 + str2
 // console.log(1+"2");
 // console.log("1"+2+2);
 // console.log(1+2+"2");
-//console.log(1+2+4+5+"565");
+console.log(1+2+4+5+"565");
 console.log(+true);
 console.log(+"");
 
