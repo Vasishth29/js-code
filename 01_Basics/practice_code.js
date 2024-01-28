62433c38c8372bad93ec21bfa87514033d9b42f5
Vasishth29 <157611630+Vasishth29@users.noreply.github.com>
2024-01-28 10:01:10 +0000

    PRACTICING IS DONE HERE

---
+++ 01_Basics/practice_code.js
@@ -0,0 +1,50 @@
+var v = 10;
+let z = "Hello";
+const PI = 3.14;
+
+v = 65487.654987
+
+//console.log(y);
+
+let person = {
+    name: "John",
+    age: 30,
+    isStudent: false
+};
+
+
+let colors = ["red","blue","green"];
+
+function add(v,g) {
+    return v * g;
+}
+
+
+
+var result = multiply(5, 10);
+//console.log(result);
+
+
+// Define the multiply function
+function multiply(a, b) {
+    return a * b;
+}
+
+// Call the multiply function
+var result = multiply(5, 10);
+// console.log(result);
+
+
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
