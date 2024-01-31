dcfe5abc86d79292bebc77d574fd18a6f9b85401
Vasishth29 <157611630+Vasishth29@users.noreply.github.com>
2024-01-31 12:57:59 +0000

    Srings

--- 01_Basics/06_strings.js
+++ 01_Basics/06_strings.js
@@ -1,8 +1,33 @@
 const name = "RadhaGopinath"
 const repoCount =  "supreme personality of godhead"
 
-console.log(name + repoCount );
+//console.log(name + repoCount );
 
 //the modern method of writing the code is :-
 
-console.log(`Hare Krishna,   ${name} is the ${repoCount}`);
+//console.log(`Hare Krishna,   ${name} is the ${repoCount}`);
+
+const gameName =  new String('RadhaGopinath')
+
+// console.log(gameName[10]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toLowerCase());
+
+// console.log(gameName.charAt(12));
+// console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0,9)
+console.log(newString);
+ 
+const anotherString = gameName.slice(-12,4)
+console.log(anotherString);
+
+const url = "https://google.com/vasishth29&njurvnVasishth"
+
+console.log(url.replace('&njurvn','__'));
+
+console.log(url.includes('anuj'));
+
+console.log(url.split('/'));
