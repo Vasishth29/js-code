f43a5de56b54f8fbf222569ed48a37998f7932e0
Vasishth29 <157611630+Vasishth29@users.noreply.github.com>
2024-01-31 12:20:43 +0000

    Strings

---
+++ 01_Basics/06_strings.js
@@ -0,0 +1,8 @@
+const name = "RadhaGopinath"
+const repoCount =  "supreme personality of godhead"
+
+console.log(name + repoCount );
+
+//the modern method of writing the code is :-
+
+console.log(`Hare Krishna,   ${name} is the ${repoCount}`);
