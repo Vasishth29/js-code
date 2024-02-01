8cf69b8f802647eac19274760e130c43dd208500
Vasishth29 <157611630+Vasishth29@users.noreply.github.com>
2024-02-01 14:20:20 +0000

    Strings

--- 01_Basics/06_strings.js
+++ 01_Basics/06_strings.js
@@ -31,3 +31,6 @@ console.log(url.replace('&njurvn','__'));
 console.log(url.includes('anuj'));
 
 console.log(url.split('/'));
+
+//notes:-
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
