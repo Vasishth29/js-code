c07c7fa83cbe0ae795c5b94e00d15fe45689b92c
Vasishth29 <157611630+Vasishth29@users.noreply.github.com>
2024-02-01 14:20:43 +0000

    Number and Maths

---
+++ 01_Basics/07_num_and_maths.js
@@ -0,0 +1,22 @@
+//+++++++++++++++++++++++++ Numbers +++++++++++++++++++++++++++++++++//
+const score = 400
+//console.log(score);
+
+const balance = new Number(566.4787)
+// console.log(balance);
+
+// console.log(balance.toString);
+// console.log(typeof balance);
+
+// console.log(balance.toFixed(10));
+
+const otherNumber = 112.65548231
+
+//console.log(otherNumber.toPrecision(1));
+
+//+++++++++++++++++++++++++++++ MATHS ++++++++++++++++++++++++++++++//
+
+//console.log(Math);
+
+console.log(Math.random());
+console.log((Math.random(0.4566)*10)+1);
